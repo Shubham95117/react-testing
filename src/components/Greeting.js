111292f7ebfe8086f1e1138f140edc7565bf38e2
shubham <shubhamchavan71195@gmail.com>
2024-08-18 12:52:23 +0530

    done testing user actions and connected components

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import Output from "./Output";
 
 const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+
+  const changeTextHandker = () => {
+    setChangedText(true);
+  };
   return (
     <div>
       <h2>Hello world!</h2>
-      <p>Its good to see you!</p>
+      {!changedText && <Output>Its good to see you!</Output>}
+      {changedText && <Output>Changed!</Output>}
+      <button onClick={changeTextHandker}>Change Text</button>
     </div>
   );
 };
